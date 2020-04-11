00e9e021ba62ce4223e4025aeaab8deb384c385f
root <root@vagrant.vm>
2020-04-11 22:54:35 +0000

    test

---
+++ cookbooks/devops/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: devops
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
---
+++ cookbooks/role_webserver/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: role_webserver
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
